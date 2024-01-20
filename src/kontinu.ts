0fd1a67f3068c8be417d4a88fd236bbc1d65304d
youkwhd <lolywk@tutanota.com>
2024-01-20 23:13:53 +0700

    feat: WIP manual check if intersecting

--- src/kontinu.ts
+++ src/kontinu.ts
@@ -3,12 +3,14 @@
 type Kontinu = {
     config: IntersectionObserverInit,
     init: (children: Element, onHit: (el: Element) => Element) => void,
+    __init: Function,
+    __isIntersecting: Function,
 }
 
 const kontinu: Kontinu = {
     config: {
         root: null,
-        rootMargin: "0px 0px 5px 0px",
+        rootMargin: "0px 0px 10px 0px",
         // threshold: 0.5, /* 50% */
     },
 
@@ -17,18 +19,39 @@ const kontinu: Kontinu = {
         new IntersectionObserver((entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
             const entry: IntersectionObserverEntry = entries[0];
 
+            console.log(entry);
+
             if (entry.isIntersecting) {
-                const lastElement: Element = onHit(children);
-                if (lastElement == undefined) {
-                    throw new Error("Kontinu: last element is undefined");
+                const element: Element = onHit(children);
+
+                if (element == undefined) {
+                    throw new Error("Kontinu: observed element is undefined");
                 }
 
                 observer.unobserve(entry.target);
-                return kontinu.init(lastElement, onHit);
+                return kontinu.init(element, onHit);
             }
         }, kontinu.config)
             .observe(children);
     },
+
+    __init: () => {
+    },
+
+    __isIntersecting: (el: HTMLElement, onHit: Function) => {
+        // assert window != null
+
+        const elementCoordinates = { x: el.clientLeft, y: el.clientTop };
+        const elementHeight = el.clientHeight;
+
+        // surpasses the element
+        if (window.scrollY > elementCoordinates.y + elementHeight) {
+        }
+
+        // intersecting the element
+        if (window.scrollY + window.innerHeight >= elementCoordinates.y) {
+        }
+    }
 };
 
 // export default kontinu;
