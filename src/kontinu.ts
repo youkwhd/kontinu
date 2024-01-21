34114ba5c3f47f59677d5d23fb760bfa84edc319
youkwhd <lolywk@tutanota.com>
2024-01-21 17:49:18 +0700

    feat: check for nullable window & document variable

--- src/kontinu.ts
+++ src/kontinu.ts
@@ -2,14 +2,18 @@
 
 type Kontinu = {
     __latestElement: HTMLElement | null,
-    __init: Function,
+    __init: (el: HTMLElement, callback: (element: HTMLElement) => HTMLElement) => void,
     __onIntersection: Function,
-}
+};
 
 const kontinu: Kontinu = {
     __latestElement: null,
 
     __init: (el: HTMLElement, callback: Function) => {
+        if (!window || !document) {
+            throw new Error("Kontinu: Could not find the global variable `window`, perhaps wait for the DOM to load.");
+        }
+
         kontinu.__latestElement = el;
         kontinu.__onIntersection(kontinu.__latestElement, callback);
 
@@ -23,7 +27,25 @@ const kontinu: Kontinu = {
     },
 
     __onIntersection: (el: HTMLElement, callback: Function) => {
-        // assert window != null
+        if (!window || !document) {
+            throw new Error("Kontinu: Could not find the global variable `window`, perhaps wait for the DOM to load.");
+        }
+
+        /* TODO: option to trigger based on:
+         * 
+         * -> trigger on the top
+         *    +------------+
+         *    |            |
+         *    |            | -> trigger on the middle
+         *    |            | 
+         *    |            |
+         *    +------------+
+         * -> trigger on the bottom
+         * 
+         * Or custom offset in px metrics.
+         */
+        const offset = 0; // px
+
         const elementCoordinates = { x: el.offsetLeft, y: el.offsetTop };
         const elementHeight = el.clientHeight;
 
@@ -33,7 +55,7 @@ const kontinu: Kontinu = {
         }
 
         // intersecting the element
-        if (window.scrollY + window.innerHeight >= elementCoordinates.y) {
+        if (window.scrollY + window.innerHeight >= elementCoordinates.y + offset) {
             const newElement: HTMLElement = callback(el);
             kontinu.__latestElement = newElement; 
             kontinu.__onIntersection(newElement, callback);
